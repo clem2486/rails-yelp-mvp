0d48508e0e7299d86174bcc3f98e2a8e85ceaefa
Clemence Roche <clem2486@gmail.com>
2020-02-19 16:51:13 +0100

    fin de l'exercice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0478642505',
+    category:     'chinese'
+  },
+  {
+    name:         'Spaghetti bolo',
+    address:      '34 rue des je parle avec les mains',
+    phone_number: '0478648913',
+    category:     'italian'
+  },
+  {
+    name:         'Fajitas party',
+    address:      '24 rue des piments forts',
+    phone_number: '0478643934',
+    category:     'french'
+  },
+  {
+    name:         'Grill meat',
+    address:      '34 rue des viandards',
+    phone_number: '0478648915',
+    category:     'french'
+  },
+  {
+    name:         'Legumes only',
+    address:      '21 rue des repas tristes',
+    phone_number: '0478643940',
+    category:     'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
